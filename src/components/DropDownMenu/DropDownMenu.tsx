65137023e5554a18cb378956a0e30d07d4523ac3
GrimFede <federico.grimaldi2006@gmail.com>
2024-07-04 17:13:34 +0200

    indentation correction

--- src/components/DropDownMenu/DropDownMenu.tsx
+++ src/components/DropDownMenu/DropDownMenu.tsx
@@ -12,53 +12,53 @@ import { MyButton } from '../Button/Button';
 import { DropDownMenuProps } from './DropDownMenuProps';
 
 export const DropDownMenu: React.FC<DropDownMenuProps> = ({
-  iconSelection = 'menu',
+    iconSelection = 'menu',
 
-  classnameDropDown,
-  classnameMenu,
-  classnameMenuIcon,
-  classnameMyButton,
-  classnameMyButtonDiv,
+    classnameDropDown,
+    classnameMenu,
+    classnameMenuIcon,
+    classnameMyButton,
+    classnameMyButtonDiv,
 
-  variantMyButton = 'text',
-  
-  options = ['text', 'text', 'text'],
-  optionsFunctionality = []
+    variantMyButton = 'text',
+
+    options = ['text', 'text', 'text'],
+    optionsFunctionality = []
 }) => {
 
-  const handleMenuClick = (index: number) => {
-    if (optionsFunctionality[index]) {
-      optionsFunctionality[index]();
-    } else {
-      console.warn(`No functionality defined for option at index: ${index}`);
-    }
-  };
+    const handleMenuClick = (index: number) => {
+        if (optionsFunctionality[index]) {
+        optionsFunctionality[index]();
+        } else {
+        console.warn(`No functionality defined for option at index: ${index}`);
+        }
+    };
 
-  const renderIcon = () => {
-    switch (iconSelection) {
-      case 'menu':
-        return <MenuIcon className={classnameMenuIcon} />;
-      case 'BOH':
-        return <SquareIcon className={classnameMenuIcon} />;
-      default:
-        return <MenuIcon className={classnameMenuIcon} />;
-    }
-  };
+    const renderIcon = () => {
+        switch (iconSelection) {
+        case 'menu':
+            return <MenuIcon className={classnameMenuIcon} />;
+        case 'BOH':
+            return <SquareIcon className={classnameMenuIcon} />;
+        default:
+            return <MenuIcon className={classnameMenuIcon} />;
+        }
+    };
 
-  return (
-    <Dropdown className={classnameDropDown}>
-      <MenuButton>
-        {renderIcon()}
-      </MenuButton>
-      <Menu className={classnameMenu}>
-        {options.map((option, index) => (
-          <MenuItem key={option} onClick={() => handleMenuClick(index)}>
-            <div className={classnameMyButtonDiv}>
-              <MyButton className={classnameMyButton} text={option} variant={variantMyButton} />
-            </div>
-          </MenuItem>
-        ))}
-      </Menu>
-    </Dropdown>
-  );
+    return (
+        <Dropdown className={classnameDropDown}>
+            <MenuButton>
+                {renderIcon()}
+            </MenuButton>
+            <Menu className={classnameMenu}>
+                {options.map((option, index) => (
+                <MenuItem key={option} onClick={() => handleMenuClick(index)}>
+                    <div className={classnameMyButtonDiv}>
+                    <MyButton className={classnameMyButton} text={option} variant={variantMyButton} />
+                    </div>
+                </MenuItem>
+                ))}
+            </Menu>
+        </Dropdown>
+    );
 };
--- src/components/DropDownMenu/DropDownMenuProps.ts
+++ src/components/DropDownMenu/DropDownMenuProps.ts
@@ -1,14 +1,14 @@
 export interface DropDownMenuProps {
     iconSelection?: 'menu' | 'BOH';
 
-  classnameDropDown?: string;
-  classnameMenuIcon?: string;
-  classnameMenu?: string;
-  classnameMyButtonDiv?: string;
-  classnameMyButton?: string;
+    classnameDropDown?: string;
+    classnameMenuIcon?: string;
+    classnameMenu?: string;
+    classnameMyButtonDiv?: string;
+    classnameMyButton?: string;
 
-  options?: string[];
-  optionsFunctionality?: (() => void)[];
+    options?: string[];
+    optionsFunctionality?: (() => void)[];
 
-  variantMyButton?: 'text' | 'outlined' | 'contained';
+    variantMyButton?: 'text' | 'outlined' | 'contained';
 }
