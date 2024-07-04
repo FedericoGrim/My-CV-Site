ca0779cb2a37f5bf2603cc37783ed25d23acf180
GrimFede <federico.grimaldi2006@gmail.com>
2024-07-04 17:41:25 +0200

    creation of _document.tsx

---
+++ src/app/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" href="@/public/favicon.ico" />
+          <meta name="description" content="Descrizione del tuo sito" />
+          <meta property="og:image" content="URL dell'immagine per l'anteprima sui social media" />
+          <meta name="og:title" content="Titolo del tuo sito" />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
