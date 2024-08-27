6efab6f1a3df45988b8251f2b497d8c22db047db
GrimFede <federico.grimaldi2006@gmail.com>
2024-08-27 19:56:05 +0200

    add of the logo in the upper right corner of the site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,8 @@ import "@/app/styles/globals.css";
 import { DropDownMenu } from "@/components/DropDownMenu/DropDownMenu";
 import { MyImage } from "@/components/Image/Image";
 
-import Image from "next/image";
+import Button from "@mui/material/Button";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,39 +22,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-
-      </head>
-
+      <head></head>
       <body className={inter.className}>
-        <div className='flex justify-end m-3'>
+        <div className="flex justify-between m-3">
+          <div className="relative">
+            <MyImage src="images/myLogo.png" alt="" width={50} height={50} />
+            <Link href="/" passHref>
+              <Button
+                className="absolute inset-0 w-full h-full bg-transparent"
+              >
+                <span className="sr-only">Torna alla Home</span>
+              </Button>
+            </Link>
+          </div>
+
           <DropDownMenu
             iconSelection="menu"
-
-            classnameDropDown=""
-            classnameMenu='my-2 bg-green-800 rounded-lg'
-            classnameMenuIcon='bg-green-800 p-1 rounded-lg text-5xl'
+            classnameDropDown="justify-end"
+            classnameMenu="my-2 bg-green-800 rounded-lg"
+            classnameMenuIcon="bg-green-800 p-1 rounded-lg text-5xl"
             classnameMyButton="text-black border-2 border-black w-24"
-            classnameMyButtonDiv='m-1'
-
-            variantMyButton='outlined'
-
+            classnameMyButtonDiv="m-1"
+            variantMyButton="outlined"
             options={["Contatti", "Progetti", "Esperienze di lavoro"]}
             optionsFunctionality={[]}
-          />
-
-          <MyImage
-            src="images/myLogo.png"
-            alt=""
-            width={60}
-            height={50}
           />
         </div>
 
         {children}
-        <footer>
-
-      </footer>
+        <footer></footer>
       </body>
     </html>
   );
