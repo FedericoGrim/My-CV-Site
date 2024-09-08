785859958263e0b97d252867e2e58bc1a9e54198
GrimFede <federico.grimaldi2006@gmail.com>
2024-09-08 23:21:20 +0200

    Improvement of Image

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -4,22 +4,26 @@ import React from 'react';
 import Image from 'next/image';
 import { ImageProps } from './ImageProps';
 
-export const MyImage: React.FC<ImageProps> = ({ 
-    src, 
-    alt = "None", 
-    width = 50, 
-    height = 50,
-    className = ""
- }) => {
+export const MyImage: React.FC<ImageProps> = ({
+  src,
+  alt = "Image description",
+  width,
+  height,
+  fill = false,  // Usa fill per indicare il layout
+  objectFit = 'cover',  // Default objectFit
+  className = "",
+  divClassName = "",
+}) => {
   return (
-    <div>
-        <Image
-            src={src}
-            alt={alt}
-            width={width}
-            height={height}
-            className={className}
-        />
+    <div className={`relative ${divClassName}`}>
+      <Image
+        src={src}
+        alt={alt}
+        // Usa fill se specificato, altrimenti usa width e height
+        {...(fill ? { fill: true } : { width, height })}
+        style={{ objectFit }}  // Usa objectFit come stile inline
+        className={className}
+      />
     </div>
-    );
+  );
 };
--- src/components/Image/ImageProps.ts
+++ src/components/Image/ImageProps.ts
@@ -1,7 +1,12 @@
 export interface ImageProps {
-    src: string;
-    alt?: string;
-    width?: number;
-    height?: number;
-    className?: string;
+    src: string;  // Il percorso dell'immagine
+    alt?: string;  // Descrizione dell'immagine
+    width?: number;  // Larghezza dell'immagine, non usato con fill
+    height?: number;  // Altezza dell'immagine, non usato con fill
+    className?: string;  // Classe CSS per l'immagine
+    divClassName?: string;  // Classe CSS per il contenitore div
+
+    // Layout dell'immagine
+    fill?: boolean;  // Indica se l'immagine deve riempire il contenitore
+    objectFit?: 'cover' | 'contain' | 'fill' | 'none' | 'scale-down';  // Come l'immagine si adatta al contenitore
 }
