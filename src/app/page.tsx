0ee34e642343852f71f10f66d6d329ee1eee2717
GrimFede <federico.grimaldi2006@gmail.com>
2024-09-09 19:16:22 +0200

    Test to see if MyImage shows the background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,11 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-0">
-      <div className="relative w-full" style={{ height: '400px' }}> {/* Definisci l'altezza desiderata */}
-        <Image
+      <div className="relative w-full" style={{ height: '400px' }}>
+        <MyImage
           src="/images/CodeBackground.png"
-          alt="Code Background"
-          fill
-          objectFit="cover"  // Mantiene le proporzioni e copre tutto il contenitore
-          className=""
+          width={50}
+          height={50}
         />
       </div>
     </main>
