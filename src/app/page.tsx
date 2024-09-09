5561e1c1b75cae92fcf6860cf360b7e9fec79f87
GrimFede <federico.grimaldi2006@gmail.com>
2024-09-09 19:39:23 +0200

    Test to fix the path of the images on the site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-0">
       <div className="relative w-full" style={{ height: '400px' }}> {/* Definisci l'altezza desiderata */}
         <Image
-          src="/images/CodeBackground.png"
+          src="/images/VaroiusImages/CodeBackground.png"
           alt="Code Background"
           fill={true}  // Usa fill invece di layout="fill"
           style={{ objectFit: 'cover' }}  // Usa objectFit come stile inline
