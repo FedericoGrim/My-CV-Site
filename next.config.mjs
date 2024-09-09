405fef62aae9bdfedbeecc28db63e1ef1942da45
GrimFede <federico.grimaldi2006@gmail.com>
2024-09-09 19:34:55 +0200

    Little fix of the possible solution for the image

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-let assetPrefix = '';
-let basePath = '';
+let assetPrefix = 'My-CV-Site';
+let basePath = 'My-CV-Site';
 
 if (isGithubActions) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');  // Ottieni il nome del repository
