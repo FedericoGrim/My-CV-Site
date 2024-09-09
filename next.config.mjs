22e9356bc17973a1f9b8492ec416238523230565
GrimFede <federico.grimaldi2006@gmail.com>
2024-09-09 19:27:03 +0200

    Test

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,22 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');  // Ottieni il nome del repository
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig = {
-    output: "export",
-    images: {
-        unoptimized: true,
-      },
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
+""
+
 import { MyImage } from "@/components/Image/Image";
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-0">
-      <div className="relative w-full" style={{ height: '400px' }}>
-        <MyImage
+      <div className="relative w-full" style={{ height: '400px' }}> {/* Definisci l'altezza desiderata */}
+        <Image
           src="/images/CodeBackground.png"
-          width={50}
-          height={50}
+          alt="Code Background"
+          fill={true}  // Usa fill invece di layout="fill"
+          style={{ objectFit: 'cover' }}  // Usa objectFit come stile inline
         />
       </div>
     </main>
