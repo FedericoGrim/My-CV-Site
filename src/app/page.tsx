6e0b810252ffec1f56d9f0706143fe1e9f2f4cf1
FedericoGrim <federico.grimaldi2006@gmail.com>
2024-09-23 22:37:04 +0200

    Add of a "Title" under my image in the home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 'use client';
 
 import { MyImage } from "@/components/Image/Image";
+import Label from "@/components/Label/Label";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-0">
-      <div className="relative w-full" style={{ height: '400px' }}>
-        {/* Prima immagine di sfondo */}
+      <div className="relative w-full">
         <MyImage 
           src="/images/CodeBackground.png" 
           alt="Code Background"
@@ -16,17 +16,22 @@ export default function Home() {
           divImageStyle={{ height: '400px' }}
         />
 
-        {/* Immagine rotonda in basso */}
         <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-[40px]">
           <MyImage 
             src="/images/FotoPlaceHolder.png" 
             alt="Round Image"
             width={250} 
             height={250} 
-            className="rounded-full border-4 border-white"
+            className="rounded-full border-4 border-customGreen"
             priority
           />
+          <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-[60px] w-[300px]">
+            <Label text="Federico&nbsp;Grimaldi" className="font-Teko text-5xl text-customGreen text-center whitespace-nowrap" font="teko" />
+          </div>
         </div>
+      </div>
+      <div>
+
       </div>
     </main>
   );
