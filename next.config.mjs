ff99757549b7449d092eef04b9fb130cb7fd33e4
FedericoGrim <federico.grimaldi2006@gmail.com>
2024-09-23 19:41:17 +0200

    Attempt to fix paths when uploaded on github action

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let basePath = '';
+let assetPrefix = '';
+
+if (isGithubActions) {
+  basePath = '/My-Cv-Site';
+  assetPrefix = '/My-Cv-Site/';
+}
+
 const nextConfig = {
   output: "export",
   images: {
-      unoptimized: true,
-    },
+    unoptimized: true, 
+  },
+  basePath,
+  assetPrefix,
 };
 
 export default nextConfig;
