d395431d7faa285ce81123e93f38cd80cef8d6f0
GrimFede <federico.grimaldi2006@gmail.com>
2024-09-26 10:00:35 +0200

    add of a little presentation label and preparation of future sections of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,9 +29,30 @@ export default function Home() {
             <Label text="Federico&nbsp;Grimaldi" className="font-Teko text-5xl text-customGreen text-center whitespace-nowrap" font="teko" />
           </div>
         </div>
-      </div>
-      <div>
 
+        <div className="absolute left-1/2 transform -translate-x-1/2 translate-y-[120px] w-[680px] text-center">
+          <Label 
+            text="Sono Federico Grimaldi, uno studente di Quinta Superiore appassionato di informatica.
+                  Con una solida conoscenza informatica e una buona abilità nel problem solving,
+                  sono pronto ad affrontare nuove sfide e ad ampliare le mie conoscenze.
+                  Sono sempre desideroso di imparare e sono motivato a cogliere ogni opportunità
+                  per crescere nel campo dell'informatica." 
+            className="text-base text-customGreen leading-6 font-regular" 
+            font="merriweather"
+          />
+        </div>
+        <div>
+          {/*obbiettivi personali*/}
+        </div>
+        <div>
+          {/*competenze*/}
+        </div>
+        <div>
+          {/*progetti più importanti*/}
+        </div>
+        <div>
+          {/*recensioni su di me*/}
+        </div>
       </div>
     </main>
   );
