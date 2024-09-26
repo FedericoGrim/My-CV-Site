450febb440b8c30b9def1197c3baa4e58114990f
FedericoGrim <federico.grimaldi2006@gmail.com>
2024-09-26 22:20:32 +0200

    completing of the footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
               <Link href="/" passHref>
                 <MyImage
                   src="images/myLogo.png"
-                  alt="Logo di Federico Grimaldi"
+                  alt="Federico Grimaldi's Logo"
                   width={50}
                   height={50}
                   className="z-0"
@@ -50,8 +50,8 @@ export default function RootLayout({
             classnameMyButton="text-black border-2 border-black w-24"
             classnameMyButtonDiv="m-1"
             variantMyButton="outlined"
-            options={["Contatti", "Progetti", "Esperienze di lavoro"]}
-            buttonsLinks={["/pages/contatti", "/pages/projects"]}
+            options={["Contacts", "Projects"]}
+            buttonsLinks={["/pages/contacts", "/pages/projects"]}
           />
         </div>
 
@@ -60,7 +60,8 @@ export default function RootLayout({
         <footer>
           <div className="flex flex-col space-y-6">
             <div className="flex flex-row space-x-6">
-              <div className="flex-1 p-6 bg-gray-800 rounded-lg text-center">
+              {/* Social Section */}
+              <div className="flex-1 p-6 bg-DarkGreen rounded-lg text-center">
                 <Label text="Social" className="text-white text-2xl font-bold mb-4" font="teko" />
 
                 <div className="space-y-4">
@@ -79,36 +80,58 @@ export default function RootLayout({
                   </div>
 
                   <div className="flex justify-center items-center space-x-2">
-                    <Label text="X/Twitter:" className="text-white text-lg" font="teko" />
-                    <Link href="https://twitter.com/esempio" passHref>
-                      <Label text="@esempio" className="text-blue-500 text-lg" font="teko" />
+                    <Label text="Instagram:" className="text-white text-lg" font="teko" />
+                    <Link href="https://www.instagram.com/federico_grima/" passHref>
+                      <Label text="federico_grima" className="text-blue-500 text-lg" font="teko" />
                     </Link>
                   </div>
                 </div>
               </div>
-              <div className="flex-1 p-6 bg-green-800 rounded-lg text-center space-y-4">
-                <Label text="Contattami" className="text-white text-3xl font-bold" font="teko"/>
+
+              {/* Download Section */}
+              <div className="w-1/4 p-6 bg-EerieBlackLighter rounded-lg text-center space-y-6">
+                <Label text="Download my CV" className="text-white text-2xl font-bold mb-4" font="teko" />
+                
+                {/* Download Buttons */}
+                <div className="flex flex-col space-y-4">
+                  <MyButton 
+                    className="text-MantisGreen border-2 border-MantisGreen py-2 rounded-lg" 
+                    text="Download CV ITA" 
+                    variant="outlined" 
+                    download="/CVs/CV_18-09-2024(ITA).pdf"
+                  />
+                  <MyButton 
+                    className="text-MantisGreen border-2 border-MantisGreen py-2 rounded-lg" 
+                    text="Download CV ENG" 
+                    variant="outlined" 
+                    download="/CVs/CV_18-09-2024(ENG).pdf"
+                  />
+                </div>
+              </div>
+
+              {/* Contact Section */}
+              <div className="flex-1 p-6 bg-DarkGreen rounded-lg text-center space-y-4">
+                <Label text="Contact Me" className="text-white text-3xl font-bold" font="teko"/>
 
                 <div className="flex justify-center space-x-2">
                   <Label text="Email:" className="text-white text-lg" font="teko"/>
-                  <Link href="mailto:federico.grimaldi2006@gmail.com?subject=Richiesta%20informazioni&body=Ciao%20Federico,%0D%0A%0D%0AScrivo%20per%20richiedere%20informazioni%20su..." passHref>
+                  <Link href="mailto:federico.grimaldi2006@gmail.com?subject=Request%20for%20information&body=Hello%20Federico,%0D%0A%0D%0AI%20am%20writing%20to%20request%20information%20about..." passHref>
                     <Label text="federico.grimaldi2006@gmail.com" className="text-blue-500 text-lg hover:underline" font="teko" />
                   </Link>
                 </div>
 
                 <div className="flex justify-center space-x-2">
-                  <Label text="Cellulare:" className="text-white text-lg" font="teko"/>
+                  <Label text="Phone:" className="text-white text-lg" font="teko"/>
                   <Label text="+39 348 342 3417" className="text-gray-300 text-lg" font="teko"/>
+                </div>
+
+                <div className="flex justify-center space-x-2">
+                  <Label text="Telegram:" className="text-white text-lg" font="teko"/>
+                  <Label text="@FedeGrim" className="text-gray-300 text-lg" font="teko"/>
                 </div>
               </div>
             </div>
-
-            <div className="text-center bg-HunterGreen rounded-lg">
-              <Label text="" className="" font=""/>
-              <MyButton className="text-black border-2 border-black w-24 bg-DarkGreen" text="Download" variant="outlined" download="/CVs/CV_18-09-2024(ITA).pdf"/>
-            </div>
           </div>
-
         </footer>
       </body>
     </html>
