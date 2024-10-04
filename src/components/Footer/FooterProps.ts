8692df7e0f65a6fd34651b57b213e231b8067b91
FedericoGrim <federico.grimaldi2006@gmail.com>
2024-10-04 16:12:19 +0200

    completion of the AdaptiveFooter

--- src/components/Footer/FooterProps.ts
+++
@@ -1,5 +0,0 @@
-export interface FooterProps {
-    screenWidth?: number;  // Il percorso dell'immagine
-    screenHeight?: number;  // Descrizione dell'immagine (reso obbligatorio per accessibilità)
-  }
-  
--- src/components/LayoutComponents/Footer/Footer.tsx
+++ src/components/LayoutComponents/Footer/Footer.tsx
@@ -1,14 +1,12 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import Image from 'next/image';
-import { FooterProps } from './FooterProps';
 import "@/app/styles/globals.css";
 import { Label } from "@/components/Label/Label";
 import Link from "next/link";
 import { MyButton } from "@/components/Button/Button";
 
-export const AdaptiveFooter: React.FC<FooterProps> = () => {
+export const AdaptiveFooter = () => {
     const [screenWidth, setScreenWidth] = useState<number>(0);
     const [screenHeight, setScreenHeight] = useState<number>(0);
 
@@ -34,7 +32,77 @@ export const AdaptiveFooter: React.FC<FooterProps> = () => {
     return (
         <>
             {screenHeight > screenWidth ? (
-                <Image src="/path/to/image.jpg" alt="Description" width={500} height={500} />
+                <div className="flex flex-col space-y-6">
+                    {/* Social Section */}
+                    <div className="flex flex-col space-y-6 p-6 bg-DarkGreen rounded-lg text-center">
+                        <Label text="Social" className="text-white text-2xl font-bold mb-4" font="teko" />
+                        <div className="space-y-4">
+                            <div className="flex justify-center items-center space-x-2">
+                                <Label text="LinkedIn:" className="text-white text-lg" font="teko" />
+                                <Link href="https://www.linkedin.com/in/federico-grimaldi-3385b9289/" passHref>
+                                    <Label text="Federico Grimaldi" className="text-blue-500 text-lg" font="teko" />
+                                </Link>
+                            </div>
+                
+                            <div className="flex justify-center items-center space-x-2">
+                                <Label text="GitHub:" className="text-white text-lg" font="teko" />
+                                <Link href="https://github.com/FedericoGrim" passHref>
+                                    <Label text="FedericoGrim" className="text-blue-500 text-lg" font="teko" />
+                                </Link>
+                            </div>
+                
+                            <div className="flex justify-center items-center space-x-2">
+                                <Label text="Instagram:" className="text-white text-lg" font="teko" />
+                                <Link href="https://www.instagram.com/federico_grima/" passHref>
+                                    <Label text="federico_grima" className="text-blue-500 text-lg" font="teko" />
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                
+                    {/* Download Section */}
+                    <div className="flex flex-col w-full p-6 bg-EerieBlackLighter rounded-lg text-center space-y-6">
+                        <Label text="Download my CV" className="text-white text-2xl font-bold mb-4" font="teko" />
+                        
+                        {/* Download Buttons */}
+                        <div className="flex flex-col space-y-4">
+                            <MyButton 
+                                className="text-MantisGreen border-2 border-MantisGreen py-2 rounded-lg" 
+                                text="Download CV ITA" 
+                                variant="outlined" 
+                                href="https://drive.google.com/file/d/13JVq1By-XVz3h-OcJ_kq4ndh3_CRdB7U/view?usp=sharing"
+                            />
+                            <MyButton 
+                                className="text-MantisGreen border-2 border-MantisGreen py-2 rounded-lg" 
+                                text="Download CV ENG" 
+                                variant="outlined" 
+                                href="https://drive.google.com/file/d/12I7PibYYqV-Of_TJviZxVc2P1WG8mK1m/view?usp=sharing"
+                            />
+                        </div>
+                    </div>
+                
+                    {/* Contact Section */}
+                    <div className="flex flex-col space-y-4 p-6 bg-DarkGreen rounded-lg text-center">
+                        <Label text="Contact Me" className="text-white text-3xl font-bold" font="teko" />
+                
+                        <div className="flex justify-center space-x-2">
+                            <Label text="Email:" className="text-white text-lg" font="teko" />
+                            <Link href="mailto:federico.grimaldi2006@gmail.com?subject=Request%20for%20information&body=Hello%20Federico,%0D%0A%0D%0AI%20am%20writing%20to%20request%20information%20about..." passHref>
+                                <Label text="federico.grimaldi2006@gmail.com" className="text-blue-500 text-lg hover:underline" font="teko" />
+                            </Link>
+                        </div>
+                
+                        <div className="flex justify-center space-x-2">
+                            <Label text="Phone:" className="text-white text-lg" font="teko" />
+                            <Label text="+39 348 342 3417" className="text-gray-300 text-lg" font="teko" />
+                        </div>
+                
+                        <div className="flex justify-center space-x-2">
+                            <Label text="Telegram:" className="text-white text-lg" font="teko" />
+                            <Label text="@FedeGrim" className="text-gray-300 text-lg" font="teko" />
+                        </div>
+                    </div>
+                </div>
             ) : (
                 <div className="flex flex-col space-y-6">
                     <div className="flex flex-row space-x-6">
