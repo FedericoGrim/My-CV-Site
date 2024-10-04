6baf5e171d3f28843a80503b96d1da83269fcd6d
FedericoGrim <federico.grimaldi2006@gmail.com>
2024-10-04 16:26:38 +0200

    change of the next.config removing non functioning code

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let basePath = '';
-let assetPrefix = '';
-
-if (isGithubActions) {
-  basePath = '/My-Cv-Site';
-  assetPrefix = '/My-Cv-Site/';
-}
-
 const nextConfig = {
   output: "export",
   images: {
-    unoptimized: true, 
-  },
-  basePath,
-  assetPrefix,
+      unoptimized: true,
+    },
 };
 
 export default nextConfig;
