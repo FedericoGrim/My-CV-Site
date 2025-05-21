0055c9abdf7fbc243ddbf7528be4c6df41f8c51a
GrimFede <federico.grimaldi2006@gmail.com>
2025-05-21 11:01:24 +0200

    Correction of the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,32 +1,39 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { MyImage } from "@/components/Image/Image";
 import { MyButton } from "@/components/Button/Button";
 import { DropDownMenu } from "@/components/DropDownMenu/DropDownMenu";
 import { Label } from "@/components/Label/Label";
 
-export default function Header() {
+import { HeaderProps } from "./HeaderProps";
+
+export default function Header({
+  options = ["Placeholder", "Placeholder"],
+  buttonsLinks = ["/", "/"],
+  classnameMyButtonDiv,
+  classnameMyButton,
+  variantMyButton,
+}: HeaderProps) {
   const [ScreenWidth, setScreenWidth] = useState(0);
 
   useEffect(() => {
     const HandleResize = () => setScreenWidth(window.innerWidth);
-
     window.addEventListener("resize", HandleResize);
     HandleResize();
-
     return () => window.removeEventListener("resize", HandleResize);
   }, []);
 
   const IsMobile = ScreenWidth < 768;
 
   return (
-    <header className="flex justify-between items-center bg-black border-b-2 border-MantisGreen p-1">
-      <div className="flex items-center bg-black border-r-2 border-MantisGreen p-1 w-[226px]">
+    <header className="flex justify-between items-center bg-DarkGreen border-b-2 border-MantisGreen p-1">
+      <div className="flex items-center p-1">
         <Link href="/" passHref>
           <MyImage
-            src="@/../images/myLogo.png"
+            src="/images/myLogo.png"
             alt="Federico Grimaldi's Logo"
             width={50}
             height={50}
@@ -35,7 +42,7 @@ export default function Header() {
         <Link href="/" passHref>
           <Label
             text="Federico Grimaldi"
-            className="flex items-center ml-2 font-Teko text-lg text-MantisGreen h-[50px]"
+            className="flex items-center ml-2 font-Teko text-2xl text-MantisGreen font-bold "
           />
         </Link>
       </div>
@@ -46,26 +53,24 @@ export default function Header() {
           classnameDropDown="bg-gray-200"
           classnameMenu="my-2 bg-MantisGreen rounded-lg"
           classnameMenuIcon="bg-MantisGreen p-1 rounded-lg text-5xl text-black"
-          classnameMyButton="text-black border-2 border-black w-24"
-          classnameMyButtonDiv="m-1"
-          variantMyButton="outlined"
-          options={["Contacts", "Projects"]}
-          buttonsLinks={["/pages/contacts", "/pages/projects"]}
+          classnameMyButton={classnameMyButton["DropDownMenuButtons"]}
+          classnameMyButtonDiv={classnameMyButtonDiv}
+          variantMyButton={variantMyButton}
+          options={options}
+          buttonsLinks={buttonsLinks}
         />
       ) : (
         <div className="flex space-x-2 mr-2 h-[50px]">
-          <MyButton
-            text="Contacts"
-            className="text-MantisGreen border-2 border-MantisGreen w-[112px]"
-            variant="outlined"
-            href="/pages/contacts"
-          />
-          <MyButton
-            text="Projects"
-            className="text-MantisGreen border-2 border-MantisGreen w-[112px]"
-            variant="outlined"
-            href="/pages/projects"
-          />
+          {options.map((option, index) => (
+            <div key={option} className={classnameMyButtonDiv}>
+              <MyButton
+                className={classnameMyButton["SeparatedButtons"]}
+                text={option}
+                variant={variantMyButton}
+                href={buttonsLinks[index]}             
+              />
+            </div>
+          ))}
         </div>
       )}
     </header>
---
+++ src/components/Header/HeaderProps.ts
@@ -0,0 +1,7 @@
+export interface HeaderProps {
+  options: string[];
+  buttonsLinks: string[];
+  classnameMyButtonDiv: string;
+  classnameMyButton: Record<string, string>;
+  variantMyButton: "outlined" | "contained" | "text";
+}
