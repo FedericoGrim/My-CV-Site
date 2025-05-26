1bda81070ab955780f0b62ec10b63f10aff120a1
GrimFede <federico.grimaldi2006@gmail.com>
2025-05-26 11:24:52 +0200

    Update of next.config to make the paths not breake when the projext is uploaded with github actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/My-CV-Site',
+  assetPrefix: '/My-CV-Site/', 
   output: "export",
   images: {
       unoptimized: true,
