c1d5291c0be94c9baa17a77b75e7f172c1d6cded
Federico Grimaldi <142935058+FedericoGrim@users.noreply.github.com>
2025-09-22 18:52:28 +0200

    Update layout.tsx from favicon.png to ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
   title: "Federico Grimaldi CV Site",
   description: "Federico Grimaldi CV",
   icons: {
-    icon: "/My-CV-Site/images/favicon.png", 
+    icon: "/My-CV-Site/images/favicon.ico", 
   },
   openGraph: {
     title: "Federico Grimaldi CV Site",
