670c4bc61b525baaf6adb8774a226bad7456553d
FedericoGrim <federico.grimaldi2006@gmail.com>
2025-12-13 17:12:00 +0100

    correction of missing exportation of LabelProps

--- src/components/Label/LabelProps.ts
+++ src/components/Label/LabelProps.ts
@@ -1,6 +1,6 @@
 import type { ReactNode } from 'react';
 
-type LabelProps = {
+export type LabelProps = {
     text?: ReactNode; //il testo che vuoi scrivere    
     className?: string; //stile del testo
     font?: string; //il font che vuoi usare tra quelli che hai importato nel file LabelFonts
