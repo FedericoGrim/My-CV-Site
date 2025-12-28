9e50f6e43b61e2ebac91ab183cbdfd5499d4b625
FedericoGrim <federico.grimaldi2006@gmail.com>
2025-12-28 23:34:45 +0100

    change of site language

--- src/app/pages/layout.tsx
+++ src/app/pages/layout.tsx
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="it">
       <head />
       <body className={inter.className}>
         <Header 
